7e50dd3ce6240b7e95b90eefc3e76ed09b08bf95
Abhishek Patwardhan <abhimpat63@gmail.com>
2024-11-12 19:13:49 -0500

    Revert "Login and Signup page created"
    This reverts commit 322bba89c504666a5e1276386635e298f3ea24e0.

--- adaptive_learning/my_app/urls.py
+++ adaptive_learning/my_app/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # This will serve the home page
-    path('login/', views.login_view, name='login'),  # For login.html
-    path('signup/', views.signup_view, name='signup'), # For Sigunp.html
 ]
--- adaptive_learning/my_app/views.py
+++ adaptive_learning/my_app/views.py
@@ -1,25 +1,5 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return render(request, 'my_app/index.html')
-
-def login_view(request):
-    return render(request, 'my_app/login.html')
-
-
-def signup_view(request):
-    return render(request, 'my_app/signup.html')
-
-# def signup_view(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)
-#             return redirect('index.html')  # Redirect to home or any other page after successful signup
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'signup.html', {'form': form})
