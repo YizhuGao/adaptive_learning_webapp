3e6fb8e5f5054b6ae10ec9d92fc6b66e252fa276
Abhishek Patwardhan <abhimpat63@gmail.com>
2025-02-20 23:35:01 -0500

    profiele update and modules page design

---
+++ adaptive_learning/my_app/Static/JS/video_modules.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const watchButtons = document.querySelectorAll(".watch-btn");
+
+    watchButtons.forEach(button => {
+        button.addEventListener("mouseover", () => {
+            button.style.transform = "scale(1.1)";
+        });
+
+        button.addEventListener("mouseout", () => {
+            button.style.transform = "scale(1)";
+        });
+    });
+});
