a00e472aef3d90db762276659c89126c43a493fa
Yash Prashant Joshi <yj44099@uga.edu>
2025-05-26 16:18:03 -0400

    Commiting latest changes of chatbot section

--- adaptive_learning/my_app/views.py
+++ adaptive_learning/my_app/views.py
@@ -1025,7 +1025,7 @@ def all_learning_videos(request):
 #     video_data = json.load(f)
 
 
-HF_API_KEY = "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"
+HF_API_KEY = "Secrete Key"
 client = InferenceClient(
     model="microsoft/Phi-3-mini-4k-instruct",
     token=HF_API_KEY
