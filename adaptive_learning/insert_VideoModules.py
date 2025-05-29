93e31b9f1f00c26c47a3124f5bcf2c1e6f174b4a
Yash Prashant Joshi <yj44099@uga.edu>
2025-05-29 16:49:34 -0400

    Fix ALLOWED_HOSTS for Render deployment

--- adaptive_learning/insert_VideoModules.py
+++ adaptive_learning/insert_VideoModules.py
@@ -1,18 +1,11 @@
-# from my_app.models import VideoModule, Topic, Subtopic
-# from django.db import transaction
-#
-# with transaction.atomic():
-#     topic_obj, created = Topic.objects.get_or_create(topic_name="Thermodynamics")
-#     subtopic_obj, created = Subtopic.objects.get_or_create(subtopic_name="Nature of temperature", topic=topic_obj)
-#     video_module = VideoModule.objects.create(
-#         title="Nature of Temperature",
-#         url="https://www.youtube.com/watch?v=96k7_7RjCpg",
-#         description="This video explains the concept of temperature and its significance in the field of thermodynamics.",
-#         topic=topic_obj,
-#         subtopic=subtopic_obj,
-#         level="Beginner"
-#     )
-#     print(f"VideoModule '{video_module.title}' added successfully!")
+import os
+import django
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adaptive_learning.settings")
+django.setup()
+
 from my_app.models import VideoModule, Topic, Subtopic
 from django.db import transaction
 
--- adaptive_learning/populate_thermodynamics_questions.py
+++ adaptive_learning/populate_thermodynamics_questions.py
@@ -1,3 +1,11 @@
+import os
+import django
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adaptive_learning.settings")
+django.setup()
+
 from my_app.models import Question, Option, Topic, Subtopic
 
 Option.objects.all().delete()
