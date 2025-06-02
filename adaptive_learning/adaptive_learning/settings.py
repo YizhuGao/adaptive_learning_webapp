0e95bec6eb008c3c5a13cc60bc1ebf53022db8da
Yash Prashant Joshi <yj44099@uga.edu>
2025-06-01 20:12:48 -0400

    Changed settings for static files path

--- adaptive_learning/adaptive_learning/settings.py
+++ adaptive_learning/adaptive_learning/settings.py
@@ -63,6 +63,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'my_app', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
